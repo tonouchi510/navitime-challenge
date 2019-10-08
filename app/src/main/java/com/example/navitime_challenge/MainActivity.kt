15f5e0a42ade7373397203ce516908cdf4cc527e
tonouchi510 <tonouchi27@gmail.com>
2019-10-08 22:41:55 +0900

    Add view pager and tablayout

--- app/src/main/java/com/example/navitime_challenge/MainActivity.kt
+++ app/src/main/java/com/example/navitime_challenge/MainActivity.kt
@@ -2,11 +2,20 @@ package com.example.navitime_challenge
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.tabs.TabLayout
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val texts = arrayOf("abc ", "bcd", "cde", "def", "efg",
+        "fgh", "ghi", "hij", "ijk", "jkl", "klm")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val tabLayout: TabLayout = findViewById(R.id.tab_layout)
+        pager.adapter = TabAdapter(supportFragmentManager,this)
+        tabLayout.setupWithViewPager(pager)
     }
 }
