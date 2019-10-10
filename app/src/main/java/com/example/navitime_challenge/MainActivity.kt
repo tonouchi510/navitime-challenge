17e257e5a601cdcf257a116815eafa4a5c3891ec
tonouchi510 <tonouchi27@gmail.com>
2019-10-11 01:20:05 +0900

    Initialize firestore client

--- app/src/main/java/com/example/navitime_challenge/MainActivity.kt
+++ app/src/main/java/com/example/navitime_challenge/MainActivity.kt
@@ -2,14 +2,20 @@ package com.example.navitime_challenge
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.navitime_challenge.adapter.TabAdapter
 import com.google.android.material.tabs.TabLayout
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private val texts = arrayOf("abc ", "bcd", "cde", "def", "efg",
-        "fgh", "ghi", "hij", "ijk", "jkl", "klm")
+    private val TAG = "MainActivity"
+    private val LIMIT = 50
+
+    private var mFirestore: FirebaseFirestore? = null
+    private var mQuery: Query? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,5 +25,25 @@ class MainActivity : AppCompatActivity() {
         pager.adapter =
             TabAdapter(supportFragmentManager, this)
         tabLayout.setupWithViewPager(pager)
+
+        initFirestore()
+    }
+
+    private fun initFirestore() {
+        mFirestore = FirebaseFirestore.getInstance()
+
+        // Get the 50 highest rated restaurants
+        val docRef = mFirestore!!.collection("orders").document("7V99VLl0mJisOBucdCVS")
+        docRef.get()
+            .addOnSuccessListener { document ->
+                if (document != null) {
+                    Log.d(TAG, "DocumentSnapshot data: ${document.data}")
+                } else {
+                    Log.d(TAG, "No such document")
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.d(TAG, "get failed with ", exception)
+            }
     }
 }
