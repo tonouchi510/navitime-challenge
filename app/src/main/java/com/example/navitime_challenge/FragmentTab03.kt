d634467820da17d28ca8c8ed5fa422199b24bf88
tonouchi510 <tonouchi27@gmail.com>
2019-10-15 17:27:30 +0900

    Add map webview

--- app/src/main/java/com/example/navitime_challenge/FragmentTab03.kt
+++ app/src/main/java/com/example/navitime_challenge/FragmentTab03.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 
@@ -18,10 +20,13 @@ class FragmentTab03: Fragment() {
     private lateinit var longitudeText: TextView
     private lateinit var mActivity: MainActivity
 
+    private lateinit var mapView: WebView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mActivity = activity as MainActivity
         location = mActivity.location
+
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -29,9 +34,15 @@ class FragmentTab03: Fragment() {
 
         latitudeText = view.findViewById(R.id.latitude_text)
         longitudeText = view.findViewById(R.id.longitude_text)
-
         latitudeText.text = location.latitude.toString()
         longitudeText.text = location.longitude.toString()
+
+        mapView = view.findViewById(R.id.map_webview)
+        mapView.webViewClient = WebViewClient()
+
+        val query: String = "latitude=" + location.latitude + "&longitude=" + location.longitude
+        mapView.loadUrl("https://asia-northeast1-navitime-challenge.cloudfunctions.net/getMap?$query")
+
         return view
     }
 
