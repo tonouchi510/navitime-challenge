55c3ab4ef01f1098fb1a08c5587d9e886e08781b
tonouchi510 <tonouchi27@gmail.com>
2019-10-29 01:24:27 +0900

    Change model to domain

--- app/src/main/java/com/example/navitime_challenge/domain/Order.kt
+++ app/src/main/java/com/example/navitime_challenge/domain/Order.kt
@@ -1,4 +1,4 @@
-package com.example.navitime_challenge.model
+package com.example.navitime_challenge.domain
 
 import com.google.firebase.Timestamp
 import com.google.firebase.firestore.IgnoreExtraProperties
