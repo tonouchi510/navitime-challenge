d80e54d8a554be755c0e2243f4d5697883f173aa
tonouchi510 <tonouchi27@gmail.com>
2019-11-04 00:22:03 +0900

    Update domain

--- app/src/main/java/com/example/navitime_challenge/domain/User.kt
+++ app/src/main/java/com/example/navitime_challenge/domain/User.kt
@@ -3,11 +3,11 @@ package com.example.navitime_challenge.domain
 import com.google.firebase.firestore.GeoPoint
 
 data class User (
-    val uid: String? = null,
-    val displayName: String? = null,
-    val email: String? = null,
-    val photoUrl: String? = null,
-    val phoneNumber: String? = null,
-    val address: String? = null,
+    val uid: String = "",
+    val displayName: String = "",
+    val email: String = "",
+    val photoUrl: String = "",
+    val phoneNumber: String = "",
+    val address: String = "",
     val geopoint: GeoPoint? = null
 )
