466e61657c54e3e088f5eb55499ce3867fc1eef6
tonouchi510 <tonouchi27@gmail.com>
2019-11-11 00:08:46 +0900

    tweak & Add enableJavascript setting to webview

--- app/src/main/java/com/example/navitime_challenge/ui/FragmentOrderList.kt
+++ app/src/main/java/com/example/navitime_challenge/ui/FragmentOrderList.kt
@@ -1,7 +1,6 @@
 package com.example.navitime_challenge.ui
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
