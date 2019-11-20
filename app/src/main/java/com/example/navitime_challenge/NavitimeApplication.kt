e52fcf6f6727479357d31c4844c2bfd44fed1638
tonouchi510 <tonouchi27@gmail.com>
2019-11-21 02:06:20 +0900

    Add worker

---
+++ app/src/main/java/com/example/navitime_challenge/NavitimeApplication.kt
@@ -0,0 +1,65 @@
+package com.example.navitime_challenge
+
+import android.app.Application
+import android.os.Build
+import androidx.work.*
+
+import com.example.navitime_challenge.work.GetOptimalShiftWorker
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import timber.log.Timber
+import java.util.concurrent.TimeUnit
+
+/**
+ * Override application to setup background work via WorkManager
+ */
+class NavitimeApplication : Application() {
+
+    private val applicationScope = CoroutineScope(Dispatchers.Default)
+
+    /**
+     * onCreate is called before the first screen is shown to the user.
+     *
+     * Use it to setup any background tasks, running expensive setup operations in a background
+     * thread to avoid delaying app start.
+     */
+    override fun onCreate() {
+        super.onCreate()
+        delayedInit()
+    }
+
+    private fun delayedInit() {
+        applicationScope.launch {
+            Timber.plant(Timber.DebugTree())
+            setupRecurringWork()
+        }
+    }
+
+    /**
+     * Setup WorkManager background job to 'fetch' new network data daily.
+     */
+    private fun setupRecurringWork() {
+
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.UNMETERED)
+            .setRequiresCharging(true)
+            .setRequiresBatteryNotLow(true)
+            .apply {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    setRequiresDeviceIdle(true)
+                }
+            }
+            .build()
+
+        val repeatingRequest = PeriodicWorkRequestBuilder<GetOptimalShiftWorker>(1, TimeUnit.DAYS)
+            .setConstraints(constraints)
+            .build()
+
+        Timber.d("WorkManager: Periodic Work request for sync is scheduled")
+        WorkManager.getInstance().enqueueUniquePeriodicWork(
+            GetOptimalShiftWorker.WORK_NAME,
+            ExistingPeriodicWorkPolicy.KEEP,
+            repeatingRequest)
+    }
+}
