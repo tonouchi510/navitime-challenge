51f5ed0a24068e8d6c7a4805a3b0daf36f406c02
tonouchi510 <tonouchi27@gmail.com>
2019-11-26 06:27:14 +0900

    Add GoogleSigninActivity

---
+++ app/src/main/java/com/example/navitime_challenge/ui/GoogleSigninActivity.kt
@@ -0,0 +1,195 @@
+package com.example.navitime_challenge.ui
+
+import android.accounts.Account
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.FragmentActivity
+import com.example.navitime_challenge.R
+import com.example.navitime_challenge.databinding.ActivitySigninBinding
+import com.google.android.gms.auth.GoogleAuthException
+import com.google.android.gms.auth.GoogleAuthUtil
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.material.snackbar.Snackbar
+import com.google.api.client.googleapis.extensions.android.accounts.GoogleAccountManager.ACCOUNT_TYPE
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import kotlinx.android.synthetic.main.activity_signin.*
+import java.io.IOException
+
+
+/**
+ * Demonstrate Firebase Authentication using a Google ID Token.
+ */
+class GoogleSignInActivity : AppCompatActivity(), View.OnClickListener {
+
+    // [START declare_auth]
+    private lateinit var auth: FirebaseAuth
+    // [END declare_auth]
+
+    private lateinit var binding: ActivitySigninBinding
+
+    private lateinit var googleSignInClient: GoogleSignInClient
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this,
+            R.layout.activity_signin
+        )
+
+        // Button listeners
+        binding.signInButton.setOnClickListener(this)
+        binding.signOutButton.setOnClickListener(this)
+        binding.disconnectButton.setOnClickListener(this)
+
+        // [START config_signin]
+        // Configure Google Sign In
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestServerAuthCode(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        // [END config_signin]
+
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        // [START initialize_auth]
+        // Initialize Firebase Auth
+        auth = FirebaseAuth.getInstance()
+        // [END initialize_auth]
+    }
+
+    // [START on_start_check_user]
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        //val currentUser = auth.currentUser
+        updateUI(null)
+    }
+    // [END on_start_check_user]
+
+    // [START onactivityresult]
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)
+                firebaseAuthWithGoogle(account!!)
+            } catch (e: ApiException) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e)
+                // [START_EXCLUDE]
+                updateUI(null)
+                // [END_EXCLUDE]
+            }
+        }
+    }
+    // [END onactivityresult]
+
+    // [START auth_with_google]
+    private fun firebaseAuthWithGoogle(acct: GoogleSignInAccount) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.id!!)
+
+        val credential = GoogleAuthProvider.getCredential(acct.idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, task.result?.credential.toString())
+                    Log.d(TAG, "signInWithCredential:success")
+                    val user = auth.currentUser
+                    updateUI(user, acct.serverAuthCode)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithCredential:failure", task.exception)
+                    Snackbar.make(main_layout, "Authentication Failed.", Snackbar.LENGTH_SHORT).show()
+                    updateUI(null)
+                }
+            }
+    }
+    // [END auth_with_google]
+
+    // [START signin]
+    private fun signIn() {
+        val signInIntent = googleSignInClient.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+    // [END signin]
+
+    private fun signOut() {
+        // Firebase sign out
+        auth.signOut()
+
+        // Google sign out
+        googleSignInClient.signOut().addOnCompleteListener(this) {
+            updateUI(null)
+        }
+    }
+
+    private fun revokeAccess() {
+        // Firebase sign out
+        auth.signOut()
+
+        // Google revoke access
+        googleSignInClient.revokeAccess().addOnCompleteListener(this) {
+            updateUI(null)
+        }
+    }
+
+    private fun updateUI(user: FirebaseUser?, authCode: String? = null) {
+        if (user != null) {
+            status.text = getString(R.string.google_status_fmt, user.email)
+            detail.text = getString(R.string.firebase_status_fmt, user.uid)
+
+            signInButton.visibility = View.GONE
+            signOutAndDisconnect.visibility = View.VISIBLE
+
+            user.getIdToken(true).addOnSuccessListener {
+                val idToken = it.token!!
+                Log.w("--------------------------", authCode!!)
+                Log.w("----------------", idToken)
+
+                val intent = Intent(application, MainActivity::class.java)
+                intent.putExtra(ID_TOKEN, idToken)
+                intent.putExtra(AUTHCODE, authCode)
+                startActivity(intent)
+            }
+
+
+        } else {
+            status.setText(R.string.signed_out)
+            detail.text = null
+
+            signInButton.visibility = View.VISIBLE
+            signOutAndDisconnect.visibility = View.GONE
+        }
+    }
+
+    override fun onClick(v: View) {
+        val i = v.id
+        when (i) {
+            R.id.signInButton -> signIn()
+            R.id.signOutButton -> signOut()
+            R.id.disconnectButton -> revokeAccess()
+        }
+    }
+
+    companion object {
+        private const val TAG = "GoogleActivity"
+        private const val RC_SIGN_IN = 9001
+        const val ID_TOKEN = "com.example.navitime_challenge.ui.TEXTDATA"
+        const val AUTHCODE = "com.example.navitime_challenge.ui.TEXTDATA"
+    }
+}
