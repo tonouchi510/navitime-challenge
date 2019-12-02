ae5dbc841b7ba56e894a66ad1f9f2686b6c92d1b
tonouchi510 <tonouchi27@gmail.com>
2019-12-02 16:43:15 +0900

    Update navitimeApiService to add query parameter

--- app/src/main/java/com/example/navitime_challenge/repository/RouteRepository.kt
+++ app/src/main/java/com/example/navitime_challenge/repository/RouteRepository.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.Transformations
 import com.example.navitime_challenge.database.RoutesDatabase
 import com.example.navitime_challenge.database.asDomainModel
 import com.example.navitime_challenge.domain.Route
+import com.example.navitime_challenge.domain.RoutePayload
 import com.example.navitime_challenge.network.NavitimeApi
 import com.example.navitime_challenge.network.asDatabaseModel
 import kotlinx.coroutines.Dispatchers
@@ -18,10 +19,16 @@ class RouteRepository(private val database: RoutesDatabase) {
         it.asDomainModel()
     }
 
-    suspend fun refreshRoutes() {
+    suspend fun refreshRoutes(payload: RoutePayload) {
         withContext(Dispatchers.IO) {
             Timber.d("refresh routes of optimal shift is called")
-            val routelist = NavitimeApi.service.getOptimalShift().await()
+            val routelist = NavitimeApi.service.getOptimalShift(
+                start = payload.start,
+                shop = payload.shop,
+                starttime = payload.starttime,
+                endtime = payload.endtime,
+                via = payload.via
+            ).await()
             database.routeDao.insertAll(routelist.asDatabaseModel())
         }
     }
--- app/src/main/java/com/example/navitime_challenge/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/navitime_challenge/viewmodel/HomeViewModel.kt
@@ -6,7 +6,8 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import com.example.navitime_challenge.database.getDatabase
+import com.example.navitime_challenge.database.getRouteDatabase
+import com.example.navitime_challenge.domain.RoutePayload
 import com.example.navitime_challenge.repository.RouteRepository
 import kotlinx.coroutines.*
 import timber.log.Timber
@@ -14,7 +15,7 @@ import java.io.IOException
 
 class HomeViewModel(application: Application) : AndroidViewModel(application) {
 
-    private val routeRepository = RouteRepository(getDatabase(application))
+    private val routeRepository = RouteRepository(getRouteDatabase(application))
 
     val routes = routeRepository.routes
 
@@ -57,7 +58,14 @@ class HomeViewModel(application: Application) : AndroidViewModel(application) {
     private fun refreshOptimalShift() {
         viewModelScope.launch {
             try {
-                routeRepository.refreshRoutes()
+                val p = RoutePayload(
+                    start = "{\"lat\":35.706822,\"lon\":139.813956}",
+                    shop = "{\"lat\":35.655392,\"lon\":139.748642}",
+                    starttime = "2018-05-01T13:00",
+                    endtime = "2018-05-01T15:55",
+                    via = "[{\"name\":\"東京都台東区秋葉原\",\"lat\":\"35.702069\",\"lon\":\"139.775327\",\"stay-time\":\"30\"},{\"name\":\"代々木公園\",\"lat\":\"35.662141\",\"lon\":\"139.771023\",\"spot\":\"02301-1300514\"},{\"stay-time\":\"30\",\"name\":\"東京\",\"node\":\"00006668\"}]"
+                )
+                routeRepository.refreshRoutes(p)
                 _eventNetworkError.value = false
                 _isNetworkErrorShown.value = false
                 Timber.d(routes.value.toString())
