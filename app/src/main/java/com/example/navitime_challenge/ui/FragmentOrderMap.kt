22a2ae644b42232dc85537fcfb07af412742da56
tonouchi510 <tonouchi27@gmail.com>
2019-12-03 22:11:26 +0900

    ワーカーでの位置情報呼び出しとかオーダーマップ表示とか（くそ）

--- app/src/main/java/com/example/navitime_challenge/ui/FragmentOrderMap.kt
+++ app/src/main/java/com/example/navitime_challenge/ui/FragmentOrderMap.kt
@@ -1,14 +1,12 @@
 package com.example.navitime_challenge.ui
 
 import android.annotation.SuppressLint
-import android.location.Location
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.example.navitime_challenge.R
@@ -17,10 +15,6 @@ import com.example.navitime_challenge.databinding.FragmentOrdermapBinding
 
 class FragmentOrderMap: Fragment() {
 
-    private lateinit var location: Location
-    private lateinit var latitudeText: TextView
-    private lateinit var longitudeText: TextView
-
     private lateinit var mapView: WebView
 
     @SuppressLint("SetJavaScriptEnabled")
@@ -28,16 +22,11 @@ class FragmentOrderMap: Fragment() {
         val binding = DataBindingUtil.inflate<FragmentOrdermapBinding>(inflater,
             R.layout.fragment_ordermap, container, false)
 
-        latitudeText = binding.latitudeText
-        longitudeText = binding.longitudeText
-        latitudeText.text = location.latitude.toString()
-        longitudeText.text = location.longitude.toString()
-
         mapView = binding.mapWebview
         mapView.webViewClient = WebViewClient()
         mapView.settings.javaScriptEnabled = true
 
-        val query: String = "latitude=" + location.latitude + "&longitude=" + location.longitude
+        val query = "latitude=35.689634&longitude=139.692101&shop=[{\"lat\":\'35.689634\',\"lon\":\'139.692101\'},{\"lat\":\'35.701429\',\"lon\":\'139.700003\'}]"
         mapView.loadUrl("https://asia-northeast1-navitime-challenge.cloudfunctions.net/getMap?$query")
 
         return binding.root
